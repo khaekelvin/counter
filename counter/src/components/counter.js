288e4405d1697226ff5033402f197a1b70c945ee
KING <kelvinksd500@gmail.com>
2023-08-28 11:23:44 +0100

    fin

--- counter/src/components/counter.js
+++ counter/src/components/counter.js
@@ -1,40 +1,34 @@
 import React, { Component } from "react";
 
-class Counter extends Component {
-  constructor() {
-    super();
+export class counter extends Component {
+  constructor(props) {
+    super(props);
     this.state = {
       count: 0,
     };
+  }
+
+  render() {
+    return (
+      <div className="top">
+        <h1>Counter = {this.state.count}</h1>
+        <button onClick={() => this.addNum()}>Add</button>
+        <button onClick={() => this.subNum()}>Subtract</button>
+      </div>
+    );
   }
 
   addNum() {
     this.setState({
       count: this.state.count + 1,
     });
-    console.log(this.state.count);
   }
 
   subNum() {
     this.setState({
       count: this.state.count - 1,
     });
-    console.log(this.state.count);
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>counter - {this.state.count}</h1>
-        <button onClick={() => this.addNum} className="add">
-          Add
-        </button>
-        <button onClick={() => this.subNum} className="sub">
-          Subtract
-        </button>
-      </div>
-    );
   }
 }
 
-export default Counter;
+export default counter;
